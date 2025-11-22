59675f3b18a520545c57c48731ce81dcc6f1ecfb
victorialee13 <victoria.hyoju.lee@gmail.com>
2025-11-21 23:34:59 -0800

    updated handeSubmit, finished all basic quote handling

--- tech-deliverable/frontend/src/App.jsx
+++ tech-deliverable/frontend/src/App.jsx
@@ -5,6 +5,8 @@ import QuoteCard from "./QuoteCard";
 function App() {
 	const [quotes, setQuotes] = useState([]);
 	const [filter, setFilter] = useState("all quotes");
+	const [name, setName] = useState("");
+	const [message, setMessage] = useState("");
 
 	useEffect(() => {
 		const url = `/api/quotes?max_age=${encodeURIComponent(filter)}`;
@@ -13,18 +15,50 @@ function App() {
 			.then((data) => setQuotes(data))
 			.catch((error) => console.error("Error fetching quotes:", error));
 	}, [filter]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append("name", name);
+		formData.append("message", message);
+
+		fetch("/api/quote", {
+			method: "POST",
+			body: formData,
+		})
+			.then((response) => response.json())
+			.then((newQuote) => {
+				setQuotes((prevQuotes) => [...prevQuotes, newQuote]);
+				setName("");
+				setMessage("");
+			})
+			.catch((error) => console.error("Error submitting quote:", error));
+	};
 	return (
 		<div className="App">
 			{/* TODO: include an icon for the quote book */}
 			<h1>Hack at UCI Tech Deliverable</h1>
 
 			<h2>Submit a quote</h2>
-			{/* TODO: implement custom form submission logic to not refresh the page */}
-			<form action="/api/quote" method="post">
+			<form onSubmit={handleSubmit}>
 				<label htmlFor="input-name">Name</label>
-				<input type="text" name="name" id="input-name" required />
+				<input
+					type="text"
+					name="name"
+					id="input-name"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					required
+				/>
 				<label htmlFor="input-message">Quote</label>
-				<input type="text" name="message" id="input-message" required />
+				<input
+					type="text"
+					name="message"
+					id="input-message"
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+					required
+				/>
 				<button type="submit">Submit</button>
 			</form>
 
